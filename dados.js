28cec138502798834820b1bdf9331f91848a6d34
Luke77983 <124450159+Luke77983@users.noreply.github.com>
2024-09-06 18:22:54 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+     {
+      titulo: "One Piece",
+      descricao: "One Piece é um popular mangá e anime japonês que acompanha as aventuras de Monkey D. Luffy, um jovem pirataque sonha em se tornar o Rei dos Piratas e encontrar o tesouro lendário, o One Piece.",
+      link: "https://onepiece.fandom.com/pt-br/wiki/One_Piece_Wiki"
+    },
+
+    {
+      titulo: "Naruto",
+      descricao: "Naruto Uzumaki é um jovem ninja da Vila da Folha que sonha em se tornar o Hokage, o líder da vila. Ele é conhecido por sua personalidade otimista e persistente, mesmo enfrentando o preconceito por ter a Nove Caudas selada dentro de si.",
+      link: "https://naruto.fandom.com/pt-br/wiki/Wiki_Naruto"
+    },
+   
+    {
+      titulo: "Dragon Ball",
+      descricao: "Son Goku é um sayajin que adora lutar e busca as Esferas do Dragão...",
+      link: "https://dragonball.fandom.com/pt-br/wiki/Dragon_Ball_Wiki"
+    }
+  ];
